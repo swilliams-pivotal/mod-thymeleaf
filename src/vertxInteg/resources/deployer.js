8b7f6436b233a8ba5548ab31aca6c0818fa0e4b3
swilliams-vmw <swilliams@vmware.com>
2013-04-12 14:29:48 +0100

    more cleanup

--- src/vertxInteg/resources/deployer.js
+++
@@ -1,9 +0,0 @@
-load('vertx.js')
-
-var config = {
-  'address': 'vertx.thymeleaf.parser'
-}
-
-vertx.deployModule('vertx.thymeleaf-v1.0', config, 1, function(id) {
-  console.log('deployed module: vertx.thymeleaf-v1.0 with id:' + id)
-})
